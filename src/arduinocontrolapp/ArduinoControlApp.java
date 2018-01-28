e8b4a3316caa1dd8fbabe7438e2cfb468c74f0f0
DanielRiveroG <danielriveroguerra@hotmail.com>
2018-01-28 23:12:36 +0000

    Digital I/O panel finished - rev

--- src/arduinocontrolapp/ArduinoControlApp.java
+++ src/arduinocontrolapp/ArduinoControlApp.java
@@ -3,13 +3,12 @@ package arduinocontrolapp;
 import java.util.ArrayList;
 import model.Instruction;
 import view.MainFrame;
-import view.MonitorPanel;
-import view.ProgramPanel;
+
 
 public class ArduinoControlApp {
 
     public static void main(String[] args) {
-        new MainFrame(new ProgramPanel(), new MonitorPanel()).setVisible(true);
+        new MainFrame().setVisible(true);
     }
     
 }
