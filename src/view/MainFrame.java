1b8fa7e649ef7a510c2f3e4faa24e02d12a0f2ca
DanielRiveroG <danielriveroguerra@hotmail.com>
2018-01-28 00:34:33 +0000

    Hardware info panel added

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -16,6 +16,7 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public MainFrame() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -40,15 +41,31 @@ public class MainFrame extends javax.swing.JFrame {
         StartBut = new javax.swing.JButton();
         PauseBut = new javax.swing.JButton();
         StopBut = new javax.swing.JButton();
+        ProgLabel = new javax.swing.JLabel();
+        InstLabel = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        DOut3 = new javax.swing.JLabel();
+        DOut5 = new javax.swing.JLabel();
+        DOut2 = new javax.swing.JLabel();
+        DOut1 = new javax.swing.JLabel();
+        DOut4 = new javax.swing.JLabel();
+        DigOutLabel = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        ConectInfoLabel = new javax.swing.JLabel();
+        ConectLabel = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
+        FileMenu = new javax.swing.JMenu();
         NewItm = new javax.swing.JMenuItem();
         OpenItm = new javax.swing.JMenuItem();
         SaveItm = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
+        ConectMenu = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        HelpMenu = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Arduino Control App");
+        setMinimumSize(new java.awt.Dimension(400, 400));
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
         ProgramList.setModel(new javax.swing.AbstractListModel<String>() {
@@ -60,22 +77,22 @@ public class MainFrame extends javax.swing.JFrame {
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         getContentPane().add(jScrollPane1, gridBagConstraints);
 
         jScrollPane2.setViewportView(InstructionTree);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         getContentPane().add(jScrollPane2, gridBagConstraints);
 
         jToolBar2.setFloatable(false);
@@ -131,24 +148,157 @@ public class MainFrame extends javax.swing.JFrame {
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         getContentPane().add(jToolBar2, gridBagConstraints);
 
-        jMenu1.setText("Archivo");
+        ProgLabel.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        ProgLabel.setText("Programa:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
+        getContentPane().add(ProgLabel, gridBagConstraints);
+
+        InstLabel.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        InstLabel.setText("Instrucciones:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
+        getContentPane().add(InstLabel, gridBagConstraints);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Información del hardware", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 12))); // NOI18N
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jPanel2.setLayout(new java.awt.GridBagLayout());
+
+        DOut3.setText("3");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DOut3, gridBagConstraints);
+
+        DOut5.setText("5");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 5;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DOut5, gridBagConstraints);
+
+        DOut2.setText("2");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DOut2, gridBagConstraints);
+
+        DOut1.setBackground(new java.awt.Color(51, 102, 0));
+        DOut1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        DOut1.setText("1");
+        DOut1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        DOut1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        DOut1.setOpaque(true);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DOut1, gridBagConstraints);
+
+        DOut4.setText("4");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DOut4, gridBagConstraints);
+
+        DigOutLabel.setText("Salidas digitales:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(DigOutLabel, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        jPanel1.add(jPanel2, gridBagConstraints);
+
+        jPanel3.setLayout(new java.awt.GridBagLayout());
+
+        ConectInfoLabel.setForeground(new java.awt.Color(255, 51, 51));
+        ConectInfoLabel.setText("No hay hardware conectado");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel3.add(ConectInfoLabel, gridBagConstraints);
+
+        ConectLabel.setText("Estado de la conexión:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel3.add(ConectLabel, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        jPanel1.add(jPanel3, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(jPanel1, gridBagConstraints);
+
+        FileMenu.setText("Archivo");
 
         NewItm.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
         NewItm.setText("Nuevo");
-        jMenu1.add(NewItm);
+        FileMenu.add(NewItm);
 
         OpenItm.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.CTRL_MASK));
         OpenItm.setText("Abrir");
-        jMenu1.add(OpenItm);
+        FileMenu.add(OpenItm);
 
         SaveItm.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
         SaveItm.setText("Guardar");
-        jMenu1.add(SaveItm);
+        FileMenu.add(SaveItm);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(FileMenu);
 
-        jMenu2.setText("Ayuda");
-        jMenuBar1.add(jMenu2);
+        ConectMenu.setText("Conexión");
+
+        jMenuItem1.setText("Configurar conexión con hardware");
+        ConectMenu.add(jMenuItem1);
+
+        jMenuBar1.add(ConectMenu);
+
+        HelpMenu.setText("Ayuda");
+        jMenuBar1.add(HelpMenu);
 
         setJMenuBar(jMenuBar1);
 
@@ -185,19 +335,34 @@ public class MainFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AddBut;
+    private javax.swing.JLabel ConectInfoLabel;
+    private javax.swing.JLabel ConectLabel;
+    private javax.swing.JMenu ConectMenu;
+    private javax.swing.JLabel DOut1;
+    private javax.swing.JLabel DOut2;
+    private javax.swing.JLabel DOut3;
+    private javax.swing.JLabel DOut4;
+    private javax.swing.JLabel DOut5;
     private javax.swing.JButton DelBut;
+    private javax.swing.JLabel DigOutLabel;
     private javax.swing.JButton EditBut;
+    private javax.swing.JMenu FileMenu;
+    private javax.swing.JMenu HelpMenu;
+    private javax.swing.JLabel InstLabel;
     private javax.swing.JTree InstructionTree;
     private javax.swing.JMenuItem NewItm;
     private javax.swing.JMenuItem OpenItm;
     private javax.swing.JButton PauseBut;
+    private javax.swing.JLabel ProgLabel;
     private javax.swing.JList<String> ProgramList;
     private javax.swing.JMenuItem SaveItm;
     private javax.swing.JButton StartBut;
     private javax.swing.JButton StopBut;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JToolBar.Separator jSeparator1;
